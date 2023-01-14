1113668d818fdbfecea1f736c17ba2128c64570f
Elvin Aslanov <rwp.primary@gmail.com>
2023-01-14 21:44:20 +0400

    Use Object-Pad

--- lib/OpenData/ShortNumberInfo.pm
+++ lib/OpenData/ShortNumberInfo.pm
@@ -1,99 +1,34 @@
 package OpenData::ShortNumberInfo;
-use v5.32;
-use HTTP::Tiny;
-use Data::Dumper qw();
-# use Encode ();
-use URI; # cpan: URI (distribution)
-use JSON; # cpan: JSON (distribution)
-# use utf8;
-use experimental qw(signatures);
-use constant PATH => qw(v1 json nrytn ShortNumberInfo);
 
-my $uri = URI -> new('https://api.opendata.az');
-my $http = HTTP::Tiny -> new();
-my $json = JSON -> new();
-my $content;
+use v5.36;
+use Object::Pad;
 
-sub new($class, $name)
-{
-  $uri -> path_segments(PATH, $name);
-  my $get = $http -> get($uri); # return value: hash reference
-  # die Data::Dumper::Dumper $get;
-  $content = $json -> decode($get -> {content});
-  return bless( {url => $uri}, $class );
+class OpenData::ShortNumberInfo {
+	use HTTP::Tiny;
+	use URI;
+	use JSON;
+
+	field $number :param //= 103;
+
+	method name ( ) {
+		# Construct API URL
+		my $uri = URI -> new( 'https://api.opendata.az' );
+		$uri -> path_segments( qw( v1 json nrytn ShortNumberInfo ), $number );
+
+		# Issue HTTP request to get the web page
+		my $http = HTTP::Tiny -> new();
+		my $response = $http -> get( $uri ); # RV: HR
+
+		# Convert JSON from HTTP response into Perl hash
+		my $json = JSON -> new();
+		my $content = $json -> decode( $response -> {content} );
+
+		unless ( defined( $content -> {StatusMessage} ) ) {
+			return $content -> {Response} -> [0] -> {Name};
+		}
+		else {
+			STDERR -> say( $content -> {StatusMessage} );
+			exit 2;
+		}
+	}
 }
-
-sub status($object)
-{
-  return $content -> {Status};
-}
-
-sub status_message($object)
-{
-  return $content -> {StatusMessage};
-}
-
-sub name($object)
-{
-  return $content -> {Response} -> [0] -> {Name};
-}
-
-sub tariff_baku($object)
-{
-  return $content -> {Response} -> [0] -> {TariffBaku};
-}
-
-sub tariff_region($object)
-{
-  return $content -> {Response} -> [0] -> {TariffRegion};
-}
-
-1;
-
-=pod
-
-=head1 NAME
-
-OpenData::ShortNumberInfo.pm - interface module to OpenData ShortNumberInfo API service
-
-=head1 SYNOPSIS
-
-use OpenData::ShortNumberInfo;
-
-my shortnumberinfo = OpenData::ShortNumberInfo -> new(169);
-
-say shortnumberinfo -> name();
-
-=head1 DESCRIPTION 
-
-=head2 METHODS
-
-=over
-
-=item status
-
-server response status code (one of OK, InputError, and ServerError)
-
-=item status_message
-
-server response status message (usually null)
-
-=item name
-
-the organization the number belongs to
-
-=item tariff_baku
-
-tarriff for calls from baku (capital) in azn (currency)
-
-=item tariff_region
-
-tarriff for calls from outside (regions) of baku in azn (currency)
-
-=back
-
-=head1 SEE ALSO
-
-L<OpenData API documentation|https://www.opendata.az/en/home/developers/>
-
-=cut
